cbd9efe1b6e6720962c729f65c233901e51357cf
chandler L <cbl0015@auburn.edu>
2020-07-13 18:18:38 -0400

    Colors integrated with JS to display differently depending on time

--- script.js
+++ script.js
@@ -48,9 +48,24 @@ for (const property in workDay) {
 
 $("button").click(function () {
     // This will save the text in the respective sibling divs 
-    
+
     value = $(this).siblings("textarea").val();
     hourString = $(this).siblings("div").text();
     saveSchedule(hourString, value);
   });
 
+  function hourNumberFromHourString(hourString) {
+    // Switching string with integers to eliminate the need to parse later on
+    switch (hourString) {
+      case "8 AM": return 8;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+    }
+  }
