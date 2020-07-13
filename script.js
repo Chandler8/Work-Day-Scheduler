873a7d3e7900bdda2127791242fd548e12956f96
chandler L <cbl0015@auburn.edu>
2020-07-13 18:10:00 -0400

    Defining variables and writing main functions in JS

--- script.js
+++ script.js
@@ -23,5 +23,34 @@ let workDay = {
   // Plug in the current date and time using Moment
   $('#date-today h5').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm a'));
 
+
+  let counter = 1;
+for (const property in workDay) {
+  // Defining the needed variables
+
+  let textEntry = "#text-entry" + counter;
+  $(textEntry).text(workDay[property]);
+  let timeId = "#time" + counter;
+  let presentHour = moment().hour();
+  let timeString = $(timeId).text();
+  let timeNumber = hourNumberFromHourString(timeString);
+  // Making the different timeblock classes correlate to past, present or future to determine styling
+  if (timeNumber < presentHour) {
+    $(textEntry).addClass("past");
+  } else if (timeNumber > presentHour) {
+    $(textEntry).addClass("future");
+  } else {
+    $(textEntry).addClass("present");
+  }
+  counter++;
+}
   
-  
+
+$("button").click(function () {
+    // This will save the text in the respective sibling divs 
+    
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
+    saveSchedule(hourString, value);
+  });
+
